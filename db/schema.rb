74fea6f5449595fb42cbf837bc3c2a8ca5604a12
Tristan <tristan.gomez@gmail.com>
2012-11-23 01:34:42 +0800

    add reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -187,8 +187,10 @@ ActiveRecord::Schema.define(:version => 20121122173013) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
