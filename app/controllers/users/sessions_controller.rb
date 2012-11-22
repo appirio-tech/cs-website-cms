06ac8439f6471d12c7eb8b2ac322529d84c03848
Tristan <tristan.gomez@gmail.com>
2012-11-23 01:09:49 +0800

    remove repeated code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,2 @@
 class Users::SessionsController < Devise::SessionsController
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
-    sign_in(resource_name, resource)
-    respond_with resource, :location => after_sign_in_path_for(resource)
-  end
 end
