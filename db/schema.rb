c26cd2b4e04c8af12da36b38da7b08b8ec6ceb68
Jeff Douglas <jeff@jeffdouglas.com>
2012-11-27 05:54:17 -0500

    migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122173813) do
+ActiveRecord::Schema.define(:version => 20121127102351) do
 
   create_table "refinery_images", :force => true do |t|
     t.string   "image_mime_type"
