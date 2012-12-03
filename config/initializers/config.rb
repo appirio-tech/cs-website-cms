14938ec7a9e46e929a4eddd6f9c597205788b34f
Tristan <tristan.gomez@gmail.com>
2012-12-03 23:29:27 +0800

    rescue from filenot found with a warning

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,5 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+begin
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+rescue Errno::ENOENT => e
+  puts 'IMPORTANT!! config/config.yml not found, you should copy config/config.example.yml to config/config.yml or else the api endpoints will not be properly setup.'
+end
