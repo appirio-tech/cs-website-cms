43a12d58898438003cb582215f520feabccd7630
Tristan <tristan.gomez@gmail.com>
2012-12-03 22:28:57 +0800

    fix for heroku (http://kent.posterous.com/omniauth-rails-3-heroku-google-apps-tempfile)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -237,7 +237,7 @@ Devise.setup do |config|
 
   require 'openid/store/filesystem'
   config.omniauth :open_id,
-    :store => OpenID::Store::Filesystem.new('/tmp'),
+    :store => OpenID::Store::Filesystem.new('./tmp'),
     :name => 'google',
     :identifier => 'https://www.google.com/accounts/o8/id',
     :require => 'omniauth-openid'
