58b04d41e797e3bbbf037aa2f6bf6c0ef0326285
Jeff Douglas <jeff@jeffdouglas.com>
2012-12-12 09:58:49 -0500

    salesforce sandbox changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,20 @@ CsWebsiteCms::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # Do care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  # to test the mailer... hard code the username & password
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => "587",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :domain         => 'gmail.com',
+    :user_name      => 'jeffdonthemic.test@gmail.com',
+    :password       => '2dreamforcepass'
+  }    
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
