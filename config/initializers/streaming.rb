a2c2f217d264cb8319dbde75420ccbc26631c43d
Jeff Douglas <jeff@jeffdouglas.com>
2012-12-19 15:09:36 -0500

    moved to vars

--- config/initializers/streaming.rb
+++ config/initializers/streaming.rb
@@ -2,11 +2,11 @@ require 'restforce'
 require 'faye'
 
 # Initialize a client with your username/password.
-client = Restforce.new :username => 'your-username',
-  :password       => 'your-password',
-  :security_token => 'your-security-token',
-  :client_id      => 'your-client-id',
-  :client_secret  => 'your-client-secret'
+client = Restforce.new :username => ENV['SFDC_PUBLIC_USERNAME'],
+  :password       => ENV['SFDC_PUBLIC_PASSWORD'],
+  :client_id      => ENV['SALESFORCE_SANDBOX_ID'],
+  :client_secret  => ENV['SALESFORCE_SANDBOX_SECRET'],
+  :host           => ENV['SFDC_HOST']
 
 client.authenticate!
 
@@ -15,6 +15,7 @@ EM.next_tick do
   # Subscribe to the PushTopic.
   client.subscribe 'AllMembers' do |message|
     id = message['sobject']['Id']
+    puts "Caught member message for #{id}"
     members = client.query("select Name from Member__c where Id = '"+id+"' limit 1")
     member = members.first
     puts "Streamed member: #{member.Name}"
@@ -27,6 +28,7 @@ EM.next_tick do
   end
   client.subscribe 'AllMails' do |message|
     id = message['sobject']['Id']
+    puts "Caught member message for #{id}"
     mails = client.query("select To__c, From__c, Subject__c, Body__c from Mail__c where Id = '"+id+"' limit 1")
     mail = mails.first
     puts "Streamed mail: To: #{mail.To__c}\n \tFrom: #{mail.From__c}\n \tSubject: #{mail.Subject__c}\n \tBody: #{mail.Body__c}"
