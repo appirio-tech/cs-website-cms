107735a1b1745762f83c07513aa81aeaaa963cfc
Jeff Douglas <jeff@jeffdouglas.com>
2013-01-15 06:09:52 -0500

    precompile assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,5 +49,7 @@ CsWebsiteCms::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.assets.initialize_on_precompile = false
+
   $stdout.sync = true
 end
