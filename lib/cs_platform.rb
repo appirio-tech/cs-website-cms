fbfa248165a3eabbd4e78c15f3d6c2b686205589
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-04 14:40:32 -0500

    initial commit

---
+++ lib/cs_platform.rb
@@ -0,0 +1,11 @@
+class CsPlatform
+
+	def self.stats
+		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/stats"))['response']
+	end
+
+	def self.leaderboard(access_token, options = {:period => nil, :category => nil, :limit => nil})
+		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
+	end
+
+end
