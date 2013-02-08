bc4212d066bc44d806b798cf2cec0e2edcd2200e
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-08 05:04:58 -0500

    added press url

--- app/models/cloudspokes_feed.rb
+++ app/models/cloudspokes_feed.rb
@@ -2,28 +2,30 @@ class CloudspokesFeed < ActiveRecord::Base
   attr_accessible :guid, :name, :published_at, :summary, :entry_type, :url
 
   def self.update_press_from_feed
-    update_from_feed('http://feeds.feedburner.com/TheCloudSpokesBlog','press')
+    update_from_feed('http://blog.cloudspokes.com/feeds/posts/default/-/news?alt=rss','press')
   end
 
   def self.update_posts_from_feed
     update_from_feed('http://feeds.feedburner.com/TheCloudSpokesBlog','posts')
   end  
 
-  def self.update_from_feed(url, type)
-    feed = Feedzirra::Feed.fetch_and_parse(url)
-    feed.sanitize_entries!
-    feed.entries.each do |entry|
-    	unless exists? :guid => entry.id
-    		create!(
-    			:name => entry.title,
-    			:summary => 'really cool summary will eventually be here',
-    			:url => entry.url,
-    			:entry_type => type,
-    			:published_at => entry.published,
-    			:guid => entry.id
-    		)
-    	end  
+  private
+
+    def self.update_from_feed(url, type)
+      feed = Feedzirra::Feed.fetch_and_parse(url)
+      feed.sanitize_entries!
+      feed.entries.each do |entry|
+      	unless exists? :guid => entry.id
+      		create!(
+      			:name => entry.title,
+      			:summary => 'WHY IS THERE NO FEED SUMMARY TEXT? DO WE WANT TO REMOVE THIS?',
+      			:url => entry.url,
+      			:entry_type => type,
+      			:published_at => entry.published,
+      			:guid => entry.id
+      		)
+      	end  
+      end
     end
-  end
 
 end
