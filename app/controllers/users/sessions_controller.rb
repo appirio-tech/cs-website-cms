1f9a535d35b5d1d505fc3e0562d144483daa0d0e
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-14 09:01:14 -0500

    debug login process

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,28 +11,43 @@ class Users::SessionsController < Devise::SessionsController
     sfdc_authentication = Account.new(User.new(:username => params[:user][:username]))
       .authenticate(params[:user][:password])
 
+    puts "===[CS-USER][LOGIN] sfdc_authentication #{sfdc_authentication.to_yaml}"
+
     # if they authenticated successfully
     if sfdc_authentication.success.to_bool
+      puts "===[CS-USER][LOGIN] authentication success"
       # see if the user exists in the database
       user = User.find_by_username(params[:user][:username])
+      puts "===[CS-USER][LOGIN] find user: #{user.to_yaml}"
       if user
+        puts "===[CS-USER][LOGIN] found the user in the db"
         # set the last refersh to yesterday so it will refresh
         user.last_access_token_refresh_at = Date.yesterday
-        user.save
-        sign_in_and_redirect(:user, user)
+        if user.save
+          puts "===[CS-USER][LOGIN] updated user last_access_token_refresh_at successfully. signing in."
+          sign_in_and_redirect(:user, user)
+        else
+          puts "===[CS-USER][LOGIN] error updating with last_access_token_refresh_at: #{user.errors.full_messages}"
+          flash[:error]  = "Sorry... there was an error logging you in. We are actively working on this issue. #{user.errors.full_messages}"
+          render action: "new" # sign_in page
+        end
       # user exists in sfdc but not in db so create a new record
       else
+        puts "===[CS-USER][LOGIN] did NOT find the user in the db"
         user =  User.new
         user.username = params[:user][:username]
         user.password = params[:user][:password]
         user.mav_hash = Encryptinator.encrypt_string params[:user][:password]
         user.last_access_token_refresh_at = Date.yesterday
+        puts "===[CS-USER][LOGIN] getting ready to save this user: #{user.to_yaml}"
         user.skip_confirmation!
         # save their record, sign them in and redirect
         if user.save
+          puts "===[CS-USER][LOGIN] user saved successfully. signing in."
           user.update_attribute(:confirmed_at, DateTime.now)
           sign_in_and_redirect(:user, user)
         else
+          puts "===[CS-USER][LOGIN] error saving new user: #{user.errors.full_messages}"
           flash[:error]  = "Sorry... there was an error creating your user account. #{user.errors.full_messages}"
           render action: "new" # sign_in page
         end
