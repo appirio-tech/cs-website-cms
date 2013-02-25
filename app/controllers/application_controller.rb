82085426cebaa7e424de04b914df70149cc4d99c
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-25 08:46:14 -0500

    redirect back to previous page after oauth login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < ActionController::Base
   private
 
     def after_sign_in_path_for(resource)
-      stored_location_for(resource) || challenges_path
+      request.env['omniauth.origin'] || stored_location_for(resource) || challenges_path
     end  
 
     def current_access_token
