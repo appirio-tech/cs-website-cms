cf0e3c4b251bc19777db28984cae63a883c36cbb
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-26 14:12:09 -0800

    added initializer for forkng pg

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,3 +7,14 @@ uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
+
+# ensure connections are disconnected and connected before forking pg
+Resque.before_fork do
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+Resque.after_fork do
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
