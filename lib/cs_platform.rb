4e5c0394d3381333c4e852e030148a1344790bfb
Jeff Douglas <jeff@jeffdouglas.com>
2013-02-26 14:16:19 -0800

    removed caching for leaderboard

--- lib/cs_platform.rb
+++ lib/cs_platform.rb
@@ -11,21 +11,15 @@ class CsPlatform
 	end	
 
 	def self.leaderboard_month(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		leaderboard_month = Rails.cache.fetch('leaderboard_month', :expires_in => ENV['MEMCACHE_EXPIRY'].to_i.minute) do
-			JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
-		end
+		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
 	end
 
 	def self.leaderboard_year(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		leaderboard_year = Rails.cache.fetch('leaderboard_year', :expires_in => ENV['MEMCACHE_EXPIRY'].to_i.minute) do
-			JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
-		end
+		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
 	end
 
 	def self.leaderboard_alltime(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		leaderboard_alltime = Rails.cache.fetch('leaderboard_alltime', :expires_in => ENV['MEMCACHE_EXPIRY'].to_i.minute) do
-			JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
-		end
+		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
 	end	
 
 end
