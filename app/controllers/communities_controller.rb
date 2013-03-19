924fd2202d614c7a0ba06f9349d6a583ce0bc20b
Jeff Douglas <jeff@jeffdouglas.com>
2013-03-18 20:37:42 -0400

    added leaderboard to community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -7,6 +7,14 @@ class CommunitiesController < ApplicationController
 		@community.leaderboard.sort! { |a,b| a['position'] <=> b['position'] }
 	end
 
+  def leaderboard
+		@community = current_community
+    # sort the leaders by position
+		@community.leaderboard.sort! { |a,b| a['position'] <=> b['position'] }
+		@points_leaderboard = @community.leaderboard
+		@referral_leaderboard = CsPlatform.leaderboard_referral(guest_access_token)
+  end
+
 	private
 
 	  def current_community
