17f258639a23672b13a1c1baa80cf052742bc0f3
Jeff Douglas <jeff@jeffdouglas.com>
2013-03-20 10:59:58 -0400

    changed output

--- app/workers/process_code_submission.rb
+++ app/workers/process_code_submission.rb
@@ -3,18 +3,20 @@ class ProcessCodeSubmission
   @queue = :process_code_submission
   def self.perform(access_token, challenge_id, membername, challenge_submission_id)
 
-  	Rails.logger.info "[INFO][Resque]==== Processing code for submission #{challenge_submission_id}" 	
+  	puts "[INFO][Resque]==== Processing code for submission #{challenge_submission_id}" 	
 
-  	Rails.logger.info "access_token #{access_token}"
-  	Rails.logger.info "challenge_id #{challenge_id}"
-  	Rails.logger.info "membername #{membername}"
-  	Rails.logger.info "challenge_submission_id #{challenge_submission_id}"
+  	puts "access_token #{access_token}"
+  	puts "challenge_id #{challenge_id}"
+  	puts "membername #{membername}"
+  	puts "challenge_submission_id #{challenge_submission_id}"
 
 		ApiModel.access_token = access_token
 		# get the participant
 		participant = Participant.find_by_member(challenge_id, membername)
 		# get this submission
 		submission = participant.find_submission(challenge_id, membername, challenge_submission_id)
+
+		puts "submission: #{submission.to_yaml}"
 
 		deliverable = SubmissionDeliverable.new
 		deliverable.type = submission.type
@@ -25,15 +27,17 @@ class ProcessCodeSubmission
 		deliverable.hosting_platform = 'Salesforce.com'
 		deliverable.language = 'Apex'
 
+		puts "deliverable: #{deliverable.to_yaml}"
+
 		# create the new deliverables record
 		results = participant.create_deliverable(challenge_id, membername, {data: deliverable})
 
 		if results.success
-			Rails.logger.info "[INFO][Resque]==== Created submission deliverable for submission #{challenge_submission_id}: #{results.message}"
+			puts "[INFO][Resque]==== Created submission deliverable for submission #{challenge_submission_id}: #{results.message}"
 			deploy_status = participant.deploy_deliverable(results.message)
-			Rails.logger.info "[INFO][Resque]==== Squirrelforce deployment status for submission deliverable #{results.message}: #{deploy_status.to_yaml}"
+			puts "[INFO][Resque]==== Squirrelforce deployment status for submission deliverable #{results.message}: #{deploy_status.to_yaml}"
 		else
-			Rails.logger.fatal "[FATAL][Resque]==== Could not create submission deliverable for submission #{challenge_submission_id}: #{results.message}"
+			puts "[FATAL][Resque]==== Could not create submission deliverable for submission #{challenge_submission_id}: #{results.message}"
 		end
   
   end
