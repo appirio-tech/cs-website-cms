2f3b45af45feacf7ad0f08cb991367a297b5b235
Jeff Douglas <jeff@jeffdouglas.com>
2013-03-22 13:09:28 -0400

    rescue for bad challenges

--- app/workers/sync_all_challenges_to_redis.rb
+++ app/workers/sync_all_challenges_to_redis.rb
@@ -6,9 +6,13 @@ class SyncAllChallengesToRedis
 		all_challenges = RestforceUtils.query_salesforce('select challenge_id__c from challenge__c')
 
 		all_challenges.each do |c|
-			Rails.logger.info "[INFO][Resque] Syncing challenge #{c.challenge_id} to redis"
-			challenge = Challenge.find c.challenge_id
-			challenge.redis_sync			
+			puts "[INFO][Resque] Syncing challenge #{c.challenge_id} to redis"
+			begin
+				challenge = Challenge.find c.challenge_id
+				challenge.redis_sync	
+			rescue Exception => e
+				puts "[INFO][Resque]Could not sync challenge #{c.challenge_id} - #{e.message}"
+			end
 		end
   end
   
