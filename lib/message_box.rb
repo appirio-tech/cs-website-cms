849c9a78b700d06670e0d3ea81fe46304bea7437
Jeff Douglas <jeff@jeffdouglas.com>
2013-03-29 07:22:42 -0400

    fixed display issue

--- lib/message_box.rb
+++ lib/message_box.rb
@@ -3,7 +3,7 @@ class MessageBox
 	attr_reader :inbox_unread, :to_messages, :from_messages
 
 	def initialize(membername, to_messages, from_messages)
-		@membername = membername
+		@membername = membername.downcase
 		@inbox_unread = 0
 		@to_messages = process_messages(to_messages, :inbox)
 		@from_messages = process_messages(from_messages, :sent)
@@ -24,14 +24,6 @@ class MessageBox
 				new_message['subject'] = m.subject
 				new_message['replies'] = m.replies.to_i
 
-				if @membername.casecmp(m.to__r.name)
-					new_message['display_user'] = m.from__r.name
-					new_message['profile_pic'] = m.from__r.profile_pic
-				elsif @membername.casecmp(m.from__r.name)
-					new_message['display_user'] = m.to__r.name
-					new_message['profile_pic'] = m.to__r.profile_pic
-				end	
-
 				if type.eql?(:inbox)
 					if @membername.casecmp(m.to__r.name)
 						new_message['status'] = m.status_to.downcase
@@ -39,9 +31,16 @@ class MessageBox
 						new_message['status'] = m.status_from.downcase
 					end
 					@inbox_unread = @inbox_unread + 1 if new_message['status'].eql?('unread')
-				elsif @type.eql?(:sent)
+
+					if @membername.eql?(m.to__r.name.downcase)
+						new_message['display_user'] = m.from__r.name
+					elsif @membername.eql?(m.from__r.name.downcase)
+						new_message['display_user'] = m.to__r.name
+					end	
+				else
 					# if we are look in the sent tab, always show the recipient's status
-					new_message['status'] = m.status_to.downcase
+					new_message['status'] = m.status_to.downcase	
+					new_message['display_user'] = m.to__r.name			
 				end			
 				processed_messages << new_message
 			end
