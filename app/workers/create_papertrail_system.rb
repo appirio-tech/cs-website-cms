18e4048d902bf33d3b42c146c78c40f1ae8f334c
Jeff Douglas <jeff@jeffdouglas.com>
2013-04-05 06:54:19 -0400

    added participant id

--- app/workers/create_papertrail_system.rb
+++ app/workers/create_papertrail_system.rb
@@ -1,8 +1,7 @@
 class CreatePapertrailSystem
   
   @queue = :create_papertrail_system
-  def self.perform(membername, email, challenge_name, challenge_participant_id)
-
+  def self.perform(membername, email, challenge_id, challenge_participant_id)
 
   	auth = {
   		:username => ENV['PAPERTRAIL_DIST_USERNAME'], 
@@ -29,12 +28,10 @@ class CreatePapertrailSystem
   	Rails.logger.info "[Resque][PT]==== Create Papertrail account: #{account_create_results}"
   	puts "[Resque][PT][PUTS]==== Create Papertrail account: #{account_create_results}"
 
-  	sleep 5
-
   	# create the system / log sender
   	payload = {
   		:id => challenge_participant_id, 
-  		:name => challenge_name, 
+  		:name => "Challenge-#{challenge_id}-#{challenge_participant_id}", 
   		:account_id => membername
   	}
   	options = { 
@@ -43,8 +40,8 @@ class CreatePapertrailSystem
   	}  	
 
   	sender_create_results = HTTParty.post("https://papertrailapp.com/api/v1/distributors/systems", options)
-  	Rails.logger.info "[Resque][PT]==== Create Papertrail account sender: #{sender_create_results}"
-  	puts "[Resque][PT][PUTS]==== Create Papertrail account sender: #{sender_create_results}"
+  	Rails.logger.info "[Resque][PT]==== Create Papertrail sender: #{sender_create_results}"
+  	puts "[Resque][PT][PUTS]==== Create Papertrail sender: #{sender_create_results}"
 
   end
   
