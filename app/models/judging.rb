7fbd2067e2091e56e7a462ca5be371b8101e9e97
Jeff Douglas <jeff@jeffdouglas.com>
2013-04-10 17:44:55 -0400

    moved to httparty calls

--- app/models/judging.rb
+++ app/models/judging.rb
@@ -4,24 +4,24 @@ class Judging < ApiModel
   end
 
   def self.judging_queue
-     naked_get('judging').map {|challenge| Challenge.new challenge}
+     http_get('judging').map {|challenge| Challenge.new challenge}
   end
 
   def self.add_judge(challenge_id, membername)
-    naked_post("judging/add", {:challenge_id => challenge_id, :membername => membername}).message
+    http_post("judging/add", {:challenge_id => challenge_id, :membername => membername}).message
   end   
 
   def self.outstanding_reviews(membername)
-  	naked_get("judging/outstanding/#{membername}")
-  end   
+  	http_get("judging/outstanding/#{membername}")
+  end     
 
   def self.participant_scorecard(participant_id, judge_membername)
-    naked_get("judging/scorecard/#{participant_id}?judge_membername=#{judge_membername}")
+    http_get("judging/scorecard/#{participant_id}", {judge_membername: judge_membername})
   end
 
   def self.save_scorecard(participant_id, answers, data) 
-    naked_put("judging/scorecard/#{participant_id}", 
+    http_put("judging/scorecard/#{participant_id}", 
       {:answers => answers, :options => data})
-  end     
+  end
 
 end
