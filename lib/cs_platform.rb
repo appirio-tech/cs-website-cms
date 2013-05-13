02170b1ab2e7baa45797d259c8065ada767be546
Jeff Douglas <jeff@jeffdouglas.com>
2013-05-13 13:02:30 -0400

    removed all reference to RestClient

--- lib/cs_platform.rb
+++ lib/cs_platform.rb
@@ -2,28 +2,28 @@ class CsPlatform
 
 	def self.stats
 		stats = Rails.cache.fetch('platform_stats', :expires_in => 30.minute) do
-			JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/stats"))['response']
+			HTTParty.get("#{ENV['CS_API_URL']}/stats")['response']
 		end
 	end
 
 	def self.tos(id)
-		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/tos/#{id}"))['response']
+		HTTParty.get("#{ENV['CS_API_URL']}/tos/#{id}")['response']
 	end	
 
 	def self.leaderboard_month(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
+		HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
 	end
 
 	def self.leaderboard_year(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
+		HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
 	end
 
 	def self.leaderboard_alltime(access_token, options = {:period => nil, :category => nil, :limit => nil})
-		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}"))['response']
+		HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
 	end	
 
 	def self.leaderboard_referral(access_token)
-		JSON.parse(RestClient.get("#{ENV['CS_API_URL']}/leaderboard/referral"))['response']
+		HTTParty.get("#{ENV['CS_API_URL']}/leaderboard/referral")['response']
 	end		
 
 end
