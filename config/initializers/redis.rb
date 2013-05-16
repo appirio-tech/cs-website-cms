93a0709647cbe45a48aa42d7eaac709a6d538af6
Jeff Douglas <jeff@jeffdouglas.com>
2013-05-16 13:00:21 -0400

    fix for unicorn and redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-ENV["REDISCLOUD_URL"] ||= 'redis://localhost:6379' 
-uri = URI.parse(ENV["REDISCLOUD_URL"])
+ENV["REDISTOGO_URL"] ||= 'redis://localhost:6379' 
+uri = URI.parse(ENV["REDISTOGO_URL"])
 
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,6 @@ preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -13,22 +12,20 @@ before_fork do |server, worker|
 
   if defined?(Resque)
     Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
   end
 
 end  
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 
   if defined?(Resque)
-    Resque.redis = ENV['REDIS_URI']
-    Rails.logger.info('Connected to Redis')
+    ENV["REDISTOGO_URL"] ||= "redis://localhost:6379" 
+    Resque.redis = ENV['REDISTOGO_URL']
   end
       
 end
