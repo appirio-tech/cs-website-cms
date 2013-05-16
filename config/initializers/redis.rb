186bbbcd25d7066497acf7132f0b5201f1026a0f
Jeff Douglas <jeff@jeffdouglas.com>
2013-05-16 09:49:25 -0400

    switched from redistogo to rediscloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-ENV["REDISTOGO_URL"] ||= 'redis://localhost:6379' 
-uri = URI.parse(ENV["REDISTOGO_URL"])
+ENV["REDISCLOUD_URL"] ||= 'redis://localhost:6379' 
+uri = URI.parse(ENV["REDISCLOUD_URL"])
 
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,9 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
+ENV["REDISCLOUD_URL"] ||= "redis://localhost:6379"
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISCLOUD_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
