24561c3e4e9190d9e4db7012dbb0405bd6669ced
Jeff Douglas <jeff@jeffdouglas.com>
2013-07-16 16:33:58 -0400

    added some code comments for madison

--- app/assets/javascripts/angular/services/madison_models.js
+++ app/assets/javascripts/angular/services/madison_models.js
@@ -3,5 +3,6 @@
 var app = angular.module('madison');
 
 app.factory('Requirement', ['$resource', function($resource) {
+    // see requirements controller
     return $resource('/requirements/:id', {id: '@id'}, {update: {method: "PUT"}});
 }]);
