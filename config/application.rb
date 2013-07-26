54d5d546955858788090930a06a48d17b60d962f
Jeff Douglas <jeff@jeffdouglas.com>
2013-07-26 16:35:20 -0400

    switched config.assets.initialize_on_precompile to false per https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module CsWebsiteCms
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # see http://refinerycms.com/guides/with-an-existing-rails-31-devise-app
     config.before_initialize do
