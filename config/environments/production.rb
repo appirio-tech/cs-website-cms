fb24f02bdd36410df3e537c4a78b9aff6feb885b
Jeff Douglas <jeff@jeffdouglas.com>
2013-08-06 09:36:02 -0400

    config changes for asset_sync to s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ CsWebsiteCms::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+  config.assets.manifest = '/website/assets'
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -43,7 +43,7 @@ CsWebsiteCms::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
