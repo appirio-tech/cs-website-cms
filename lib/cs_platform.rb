4d031c6bd1edd90467e1019c2f3d9375b40e8351
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-04 16:33:36 -0400

    tweaked leaderboard calls

--- lib/cs_platform.rb
+++ lib/cs_platform.rb
@@ -1,7 +1,7 @@
 class CsPlatform
 
   def self.stats
-    stats = Rails.cache.fetch('platform_stats', :expires_in => 30.minute) do
+    stats = Rails.cache.fetch('platform_stats', :expires_in => 60.minute) do
       HTTParty.get("#{ENV['CS_API_URL']}/stats")['response']
     end
   end
@@ -10,24 +10,9 @@ class CsPlatform
     HTTParty.get("#{ENV['CS_API_URL']}/tos/#{id}")['response']
   end	
 
-  def self.leaderboard_all(access_token, options = {:limit => nil})
+  def self.leaderboards(access_token, options = {:limit => nil})
     HTTParty.get("#{ENV['CS_API_URL']}/leaderboard_all?#{options.to_param}")['response']
-  end	
-
-  # depracated -- no longer being used. use leaderboard_all instead
-  def self.leaderboard_month(access_token, options = {:period => nil, :category => nil, :limit => nil})
-    HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
   end
-
-  # depracated -- no longer being used. use leaderboard_all instead
-  def self.leaderboard_year(access_token, options = {:period => nil, :category => nil, :limit => nil})
-    HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
-  end
-
-  # depracated -- no longer being used. use leaderboard_all instead
-  def self.leaderboard_alltime(access_token, options = {:period => nil, :category => nil, :limit => nil})
-    HTTParty.get("#{ENV['CS_API_URL']}/leaderboard?#{options.to_param}")['response']
-  end	
 
   def self.leaderboard_referral(access_token)
     HTTParty.get("#{ENV['CS_API_URL']}/leaderboard/referral")['response']
