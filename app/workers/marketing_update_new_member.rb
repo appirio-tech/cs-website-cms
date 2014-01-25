e978fdeda4137e84c51da4a83d86f1f28bd74916
Jeff Douglas <jeff@jeffdouglas.com>
2014-01-25 10:58:46 -0500

    adjusted formatting

--- app/workers/marketing_update_new_member.rb
+++ app/workers/marketing_update_new_member.rb
@@ -3,10 +3,10 @@ class MarketingUpdateNewMember
   @queue = :marketing_update_queue
   def self.perform(access_token, membername, campaign_source, campaign_medium, campaign_name)
 
-  	Rails.logger.info "[INFO][Resque]==== Updating #{membername} with marketing info"
+    Rails.logger.info "[INFO][Resque]==== Updating #{membername} with marketing info"
 
-		ApiModel.access_token = access_token
-  	results = Account.new(User.new(:username => membername)).process_marketing(campaign_source,campaign_medium,campaign_name)
+    ApiModel.access_token = access_token
+    results = Account.new(User.new(:username => membername)).process_marketing(campaign_source,campaign_medium,campaign_name)
 
     log_type = 'FATAL'
     log_type = 'INFO' if results.success
